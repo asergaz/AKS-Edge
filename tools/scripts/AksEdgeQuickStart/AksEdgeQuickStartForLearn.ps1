93ad3e6dfbcc436ca2c6f1ad88213a6c0a989708
António Sérgio Azevedo <22173500+asergaz@users.noreply.github.com>
2023-12-06 14:04:10 +0000

    using azure file share and AksEdgeProductUrl

--- tools/scripts/AksEdgeQuickStart/AksEdgeQuickStartForLearn.ps1
+++ tools/scripts/AksEdgeQuickStart/AksEdgeQuickStartForLearn.ps1
@@ -9,10 +9,11 @@ param(
     [String] $Tag,
     [Switch] $UseK8s,
     [Switch] $WindowsNode,
-    [Switch] $ArcEnabled
-    [String] $storageAccountName,
-    [String] $storageAccountKey,
-    [String] $storageAccountFile,
+    [Switch] $ArcEnabled,
+    [Switch] $AzureFileShare,
+    [String] $StorageAccountName,
+    [String] $StorageAccountKey,
+    [String] $StorageAccountFile
 )
 
 #Requires -RunAsAdministrator
@@ -66,26 +67,10 @@ if ($UseK8s) {
 }
 
 #Mount the Azure File Share
-$skipAzureFileShare = $false
-if ([string]::IsNullOrEmpty($storageAccountName)) {
-    Write-Host "Warning: Require storageAccountName for Azure File Share" -ForegroundColor Cyan
-    $skipAzureFileShare = $true
-}
-if ([string]::IsNullOrEmpty($storageAccountKey)) {
-    Write-Host "Warning: Require storageAccountKey for Azure File Share" -ForegroundColor Cyan
-    $skipAzureFileShare = $true
-}
-if ([string]::IsNullOrEmpty($storageAccountFile)) {
-    Write-Host "Warning: Require storageAccountFile for Azure File Share" -ForegroundColor Cyan
-    $skipAzureFileShare = $true
-}
-if($skipAzureFileShare)
-{
-    Write-Host "Azure File Share will be skipped as required details are not available" -ForegroundColor Yellow
-} else {
+if($AzureFileShare){
     # Save the password so the drive will persist on reboot
-    cmd.exe /C "cmdkey /add:``"$storageAccountName.file.core.windows.net``" /user:``"localhost\$storageAccountName``" /pass:``"$storageAccountKey``""
-    # Mount the drive
+    cmd.exe /C "cmdkey /add:`"$storageAccountName.file.core.windows.net`" /user:`"localhost\$storageAccountName`" /pass:`"$storageAccountKey`""
+    # Mount the drive with the letter Z
     New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$storageAccountName.file.core.windows.net\$storageAccountFile" -Persist
 }
 
@@ -94,8 +79,8 @@ $aideuserConfig = @"
 {
     "SchemaVersion": "1.1",
     "Version": "1.0",
-    "AksEdgeProduct": "$productName",
-    "AksEdgeProductUrl": "",
+    "AksEdgeProduct": "",
+    "AksEdgeProductUrl": "Z:\AksEdge.msi",
     "Azure": {
         "SubscriptionName": "",
         "SubscriptionId": "$SubscriptionId",
@@ -255,25 +240,6 @@ if ($azcfg.Auth.Password) {
 
 # Download, install and deploy AKS EE 
 Write-Host "Step 3: Download, install and deploy AKS Edge Essentials"
-# when azure file share is available we will use the msi file from there
-if(!$skipAzureFileShare){
-    if($WindowsNode){
-        Expand-Archive -Path "Z:\windows-node.zip" -DestinationPath "Z:\" -Force
-        $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i Z:\k3s.msi ADDLOCAL=CoreFeature,WindowsNodeFeature /li $installDir\k3s-install.log /qn" -PassThru
-    }
-    else{
-        $process = Start-Process -FilePath "msiexec.exe" -ArgumentList "/i Z:\k3s.msi /li $installDir\k3s-install.log /qn" -PassThru
-    }
-    $process.WaitForExit()
-    $exitCode = $process.ExitCode
-    if ($exitCode -eq 0) {
-        Write-Host "Installing k3s.msi from Azure File Share succeeded." -ForegroundColor Green
-    } else {
-        Write-Host "Installation of k3s.msi from Azure File Share failed with exit code $exitCode." -ForegroundColor Red
-        Stop-Transcript | Out-Null
-        exit -1
-    }
-}
 # invoke the workflow, the json file already updated above.
 $retval = Start-AideWorkflow -jsonFile $aidejson
 if ($retval) {
